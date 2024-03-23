e68cd187bb06080c3506e6287d4aa9d52e97b0fd
maleahy <maleahyee@gmail.com>
2024-03-22 20:46:11 -0600

    comments2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,15 +16,16 @@ namespace Mission11_Yee.Controllers
         public IActionResult Index(int page = 1)
         {
             int pageSize = 10; // Set the number of items per page
-            var books = _repo.Books;
+            var books = _repo.Books; //Var for all books
 
             var pagingInfo = new PagingInfo
             {
-                CurrentPage = page,
-                ItemsPerPage = pageSize,
-                TotalItems = books.Count()
+                CurrentPage = page, //page user is on
+                ItemsPerPage = pageSize, //How many items per page
+                TotalItems = books.Count() //Number of books
             };
 
+            //pagination logic
             var booksListViewModel = new BooksListViewModel
             {
                 Books = books
